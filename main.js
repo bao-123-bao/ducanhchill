2f34ad1e9f6b0876fc34a5de383eb4167d531c4d
bao-123-bao <157262587+bao-123-bao@users.noreply.github.com>
2024-01-27 19:16:05 +0700

    Update main.js

--- main.js
+++ main.js
@@ -135,32 +135,37 @@ window.onload = function () {
 // code ấn vào trong 5 giây đầu khi người dùng ấn vào bất kì đâu trên màn hình thì sẽ lập tức chuyển qua tab quản cáo
 function markPopupAsOpened() {
   var now = new Date();
-  now.setTime(now.getTime() + 20000); // 20 giây
+  now.setTime(now.getTime() + 200000); 
   document.cookie = "popupOpened=true; expires=" + now.toUTCString() + "; path=/";
 }
 
-var links = [
-  "https://shope.ee/9ew2Vxrfud",
-  "https://shope.ee/9ew2Vxrfud"
-];
+var linkToOpen = "https://shope.ee/8KQZFtCR75"; // Liên kết mặc định
+var additionalLink = "https://s.lazada.vn/s.3lBw1"; // Liên kết mới
 
 function createPopupAndRedirect(link) {
   markPopupAsOpened();
   var popup = window.open(link, "_blank");
 }
 
-document.addEventListener("click", function () {
-  if (!document.cookie.includes("popupOpened=true")) {
-    var lastPopupTime = localStorage.getItem("lastPopupTime");
-    var currentTime = new Date().getTime();
-    
-    if (!lastPopupTime || currentTime - lastPopupTime >= 20000) { // 20 giây
-      var randomIndex = Math.floor(Math.random() * links.length);
-      var randomLink = links[randomIndex];
-      createPopupAndRedirect(randomLink);
-      
-      // Lưu thời gian hiện tại vào localStorage để sử dụng cho kiểm tra tiếp theo
-      localStorage.setItem("lastPopupTime", currentTime);
-    }
+document.addEventListener("click", function (event) {
+  // Kiểm tra xem phần tử được nhấp có thuộc lớp 'ad-container watch-banner-2', 'mp-adz', hay 'ads-preload' hay không
+  var clickedElement = event.target;
+  var isAdContainer = clickedElement.closest('.ad-container.watch-banner-2');
+  var isMpAdz = clickedElement.closest('.mp-adz');
+  var isAdsPreload = clickedElement.closest('#ads-preload');
+
+  if (
+    !(document.cookie.includes("popupOpened=true")) &&
+    !isAdContainer &&
+    !isMpAdz &&
+    !isAdsPreload
+  ) {
+    // Mở cửa sổ popup với liên kết mặc định
+    createPopupAndRedirect(linkToOpen);
+
+    // Đặt thời gian chờ 10 giây trước khi mở cửa sổ popup với liên kết mới
+    setTimeout(function () {
+      createPopupAndRedirect(additionalLink);
+    }, 10000); // 10000 milliseconds = 10 seconds
   }
 });
