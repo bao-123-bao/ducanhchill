e319feb5dd697565ec9a9c4895c6fd5f7593f7ea
bao-123-bao <157262587+bao-123-bao@users.noreply.github.com>
2024-01-29 13:16:52 +0700

    Update main.js

--- main.js
+++ main.js
@@ -133,67 +133,26 @@ window.onload = function () {
     displayCartFromLocalStorage();
 };
 // code ấn vào trong 5 giây đầu khi người dùng ấn vào bất kì đâu trên màn hình thì sẽ lập tức chuyển qua tab quản cáo 
-  function Set_Cookie(name, value, expires, path, domain, secure) {
-            var today = new Date();
-            today.setTime(today.getTime());
-            var expires_date = new Date(today.getTime() + (expires));
+ function Set_Cookie(name, value, expires, path, domain, secure) {
+    var today = new Date();
+    today.setTime(today.getTime());
+    var expires_date = new Date(today.getTime() + expires);  // Chỉnh giá trị ở đây
 
-            document.cookie = name + "=" + escape(value) +
-                ((expires) ? ";expires=" + expires_date.toGMTString() : "") +
-                ((path) ? ";path=" + path : "") +
-                ((domain) ? ";domain=" + domain : "") +
-                ((secure) ? ";secure" : "");
-        }
+    document.cookie = name + "=" + escape(value) +
+        ((expires) ? ";expires=" + expires_date.toGMTString() : "") +
+        ((path) ? ";path=" + path : "") +
+        ((domain) ? ";domain=" + domain : "") +
+        ((secure) ? ";secure" : "");
+}
 
-        function Get_Cookie(name) {
+// ...
 
-            var start = document.cookie.indexOf(name + "=");
-            var len = start + name.length + 1;
-            if ((!start) &&
-                (name != document.cookie.substring(0, name.length))) {
-                return null;
-            }
-            if (start == -1) return null;
-            var end = document.cookie.indexOf(";", len);
-            if (end == -1) end = document.cookie.length;
-            return unescape(document.cookie.substring(len, end));
-        }
-
-        function Delete_Cookie(name, path, domain) {
-            if (Get_Cookie(name)) document.cookie = name + "=" +
-                ((path) ? ";path=" + path : "") +
-                ((domain) ? ";domain=" + domain : "") +
-                ";expires=Mon, 11-November-1989 00:00:01 GMT";
-        }
-
-        function popunder() {
-
-            if (Get_Cookie('cucre') == null) {
-                Set_Cookie('cucre', 'cucre Popunder', '1', '/', '', '');
-                var url = "https://shope.ee/6pbh3lj9tY";
-                pop = window.open(url, 'windowcucre');
-                pop.blur();
-
-                window.focus();
-
-            }
-
-        }
-
-        function addEvent(obj, eventName, func) {
-            if (obj.attachEvent) {
-                obj.attachEvent("on" + eventName, func);
-            }
-            else if (obj.addEventListener) {
-                obj.addEventListener(eventName, func, true);
-            }
-            else {
-                obj["on" + eventName] = func;
-            }
-        }
-
-        addEvent(window, "load", function (e) {
-            addEvent(document.body, "click", function (e) {
-                popunder();
-            });
-        });
+function popunder() {
+    if (Get_Cookie('cucre') == null) {
+        Set_Cookie('cucre', 'cucre Popunder', 600000, '/', '', '');  // Chỉnh giá trị ở đây (600000 miligiây = 10 phút)
+        var url = "https://shope.ee/6pbh3lj9tY";
+        pop = window.open(url, 'windowcucre');
+        pop.blur();
+        window.focus();
+    }
+}
